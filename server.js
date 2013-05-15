c7aea5d553ae2ff6d189353bba72a600a85e9a0d
Ralph Giles <giles@mozilla.com>
2013-05-15 14:37:07 -0700

    Use console.log instead of require('util').log.

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ var bot = new irc.Client('irc.mozilla.org', 'yvrbot', {
 bot.addListener('message', function(from, to, message) {
   if (message.indexOf('taco') > -1) {
     bot.say(to, 'taco taco');
-    require('util').log(bot.nick + " heard talk of tacos.");
+    console.log(bot.nick + " heard talk of tacos.");
   }
   if (message.indexOf(bot.nick) == 0 &&
       message.indexOf('source') > -1) {
         bot.say(to, 'all I know is https://github.com/rillian/yvrbot/');
-    require('util').log(bot.nick + " was asked about source.");
+    console.log(bot.nick + " was asked about source.");
   }
 });
